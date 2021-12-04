ad83e24066344576aadde27faa1723c56800c584
Ryan Ringer <RyanARinger1@gmail.com>
2021-12-03 23:12:10 -0800

    Update AsteroidsGameTest.java

--- src/test/java/AsteroidsGameTest.java
+++ src/test/java/AsteroidsGameTest.java
@@ -6,7 +6,7 @@ public class AsteroidsGameTest {
 
 	@Test
 	public void testKillPlayer() {
-		System.out.println("Hello World_1dfgh_xfghsr2");
+		System.out.println("Hello World_2");
 		assertEquals(true, true);
 	}
 
