afd4eed7bf1da0b1a873707f8a41b0d801499e13
Ryan Ringer <RyanARinger1@gmail.com>
2021-12-03 23:19:24 -0800

    dgfasg

--- src/test/java/AsteroidsGameTest.java
+++ src/test/java/AsteroidsGameTest.java
@@ -6,7 +6,7 @@ public class AsteroidsGameTest {
 
 	@Test
 	public void testKillPlayer() {
-		System.out.println("Hello World_4");
+		System.out.println("Hello World_6");
 		assertEquals(true, true);
 	}
 
