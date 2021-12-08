5093c8e21dd61b6bdef59b2cc81273ff3cfb344c
Ryan <ryanaringer1@gmail.com>
2021-12-07 23:47:45 -0800

    demo 1212

--- src/test/java/AsteroidsGameTest.java
+++ src/test/java/AsteroidsGameTest.java
@@ -6,7 +6,7 @@ public class AsteroidsGameTest {
 
 	@Test
 	public void testKillPlayer() {
-		System.out.println("Hello World Demonstration 3");
+		System.out.println("Hello World Demonstration 4");
 		//
 		assertEquals(true, true);
 	}
