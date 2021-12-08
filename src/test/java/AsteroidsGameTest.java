8a6c71cdcc08a2fbec1176b78c2c9d769d895fea
Ryan <ryanaringer1@gmail.com>
2021-12-08 03:12:01 -0800

    awsdfasd

--- src/test/java/AsteroidsGameTest.java
+++ src/test/java/AsteroidsGameTest.java
@@ -6,8 +6,7 @@ public class AsteroidsGameTest {
 
 	@Test
 	public void testKillPlayer() {
-		System.out.println("Hello World Demonstration 4");
-		//asdf
+		System.out.println("Hello World Demonstration and Integration Test");
 		assertEquals(true, true);
 	}
 
